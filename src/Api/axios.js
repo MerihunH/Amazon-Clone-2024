8ad89250775015aaef7de113ef0da36eae9d2742
“[ <153067235+MerihunH@users.noreply.github.com>
2024-02-18 02:34:40 -0800

    backend

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-cd15e/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-cd15e/us-central1/api",
+  baseURL: "https://api-an55ns775a-uc.a.run.app",
 });
 
 export { axiosInstance };
